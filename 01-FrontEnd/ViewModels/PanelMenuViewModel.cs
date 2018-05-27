530c3662cbf192b573b128dd4b53f1176232d8e4
Carlos <juanhernandezcastro.c@gmail.com>
2018-05-27 18:14:58 -0500

    The entire project is added

---
+++ 01-FrontEnd/ViewModels/PanelMenuViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrontEnd.ViewModels
+{
+    public class PanelMenuViewModel
+    {
+        public Enums.PanelMenu id { get; set; }
+        public string Name { get; set; }
+        public string Href { get; set; }
+        public string Icon { get; set; }
+        public bool Selected { get; set; }
+        public int Order { get; set; }
+
+        public static IEnumerable<PanelMenuViewModel> GetAll(Enums.PanelMenu id)
+        {
+            var result = new List<PanelMenuViewModel> {
+                new PanelMenuViewModel
+                {
+                    id = Enums.PanelMenu.Home,
+                    Name = "Home",
+                    Href = "/panel",
+                    Icon = "fa-home",
+                    Order = 1
+                },
+                new PanelMenuViewModel
+                {
+                    id = Enums.PanelMenu.Categories,
+                    Name = "Categories",
+                    Href = "/panel/categories",
+                    Icon = "fa fa-object-group",
+                    Order = 2
+                },
+                new PanelMenuViewModel
+                {
+                    id = Enums.PanelMenu.Courses,
+                    Name = "Courses",
+                    Href = "/panel/courses",
+                    Icon = "fa-book",
+                    Order = 3
+                },
+                new PanelMenuViewModel
+                {
+                    id = Enums.PanelMenu.Users,
+                    Name = "Users",
+                    Href = "/panel/users",
+                    Icon = "fa-user",
+                    Order = 4
+                },
+            };
+
+            return result.Select(x => new PanelMenuViewModel {
+                id = x.id,
+                Name = x.Name,
+                Href = x.Href,
+                Icon = x.Icon,
+                Selected = x.id == id
+            });
+        }
+    }
+}
